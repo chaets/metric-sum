19adfc19678b6459ff7bf05ddb0cc612012cb232
CSP19SCM54G <cgupta5@hawk.iit.edu>
2019-09-11 23:44:03 -0500

    Final Submission

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # metric-sum
-BSC
+## BSC
+
+There are only 2 functions I have used which basically do all the functionality 
+
+#### Funtion to store real time data with time stamps in dictionary
+def timestamp_dict(data):
+#### function to check latest 2 hours data
+def sum_2hours()
+
+Post request example
+![](post.JPG)
+Get Request Example
+![](get.JPG)
+
+Limitation:
+1. It just support only one variable to store and show sum, but we can increase as per the usage
+
+
--- final.py
+++ final.py
@@ -1,76 +1,48 @@
-
 import time
 from flask import Flask, jsonify, request  # import objects from the Flask model
 app = Flask(__name__)  # define app using Flask
 
-# in_memory = dict()
-in_mem_dict= {}
-#initial idea to turn this work but failed
 
-# # def add(val, data):
-#     curr = time.localtime()
-#     data[curr] = val
-#     print(data)
-#
-#
-# def parse_data (data):
-#     curr = time.localtime()
-#     total = 0
-#     print('test: ', list(data.values())[0]['value'])
-#     # new_data = dict()
-#     for key in data:
-#         # key = int(keys)
-#         if (key.tm_year == curr.tm_year and key.tm_mon == curr.tm_mon and key.tm_mday == curr.tm_mday):
-#             if ((curr.tm_hour - key.tm_hour) < 1 and curr.tm_min - key.tm_min < 1 and (curr.tm_sec - key.tm_sec) < 20):
-#                 total = total + list(data.values())[0]['value']
-#     return total
-
-#second idea to work little bit more needs to change and I think this will work, something small mistake I am doing over here.
-
+in_mem_dict = {}
+# Funtion to store real time data with time stamps in dictionary
 def timestamp_dict(data):
     ts = time.time()
-    # in_mem_dict.update(ts = data )
+
     in_mem_dict[ts] = data
 
-def sum_2hours():
+def sum_2hours(): # function to check lastest 2 hours data
     sum1 = 0
-    # values = [v for k,v in in_mem_dict.items() if k >= (time.localtime().tm_hour- 2)]
     for k, v in in_mem_dict.items():
-        if k >= (time.localtime().tm_hour - 2):
-            sum1 = sum1 + int(in_mem_dict[k])
-    print(sum1)
-    return sum1
+        print(time.time(), k)
+        if 7200 >= (time.time() - k):
+            sum1 = sum1 + in_mem_dict[k]
+
+    return int(sum1)
 
 
-values = [{'value': 5}, {'value': 6}, {'value': 7}]
-
-
-
+values = []
+#function to show if there is any value in metric url
 @app.route('/metric/', methods=['GET'])
 def returnAlla():
-	return jsonify({'values' : values})
+	return jsonify(values)
 
 
-
+#function to POST all values using POST request in metric url
 @app.route('/metric/', methods=['POST'])
 def try_f():
     if request.method == "POST":
 
         value = {'value': request.json['value']}
         values.append(value)
-        timestamp_dict(value)
-        # add(value, in_memory)
-        return jsonify({'value': values})
+        timestamp_dict(value['value'])
+        return jsonify(values)
 
 
-
+#function to show if there is show sum of all latest 2 hours data from metric url to /metric/activeuser/sum'
 @app.route('/metric/activeuser/sum', methods=['GET'])
 def returnAll():
-    # sum = 0
     if request.method == "GET":
-        # sum = parse_data(in_memory)
         total = sum_2hours()
-
     return jsonify({'sum': total})
 
 
